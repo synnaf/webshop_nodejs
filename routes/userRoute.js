d7470e9f2e489a3834b0fe2e70801204469b15a7
mariaekmaan1991 <mariaekmaan@hotmail.com>
2020-03-06 22:14:24 +0100

    lägg till användare

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,19 +1,57 @@
 const express = require('express');
 const router = express.Router();
-// const User = require('../model/user');
+const bcrypt = require('bcrypt');
+const UserInfoModel = require('../model/user');
 const constant = require('../constant');
+
+
+router.get(constant.ROUTE.createUser, (req, res) => {
+    res.status(200).render(constant.VIEW.createUser);
+
+})
+router.post(constant.ROUTE.createUser, async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.userPassWord, salt)
+    await new UserInfoModel({
+        userMail: req.body.userMail,
+        userPassWord: hashPassword,
+        userAddress: req.body.userAddress,
+        userFirstName: req.body.userFirstName,
+        userLastName: req.body.userLastName,
+    }).save()
+    res.redirect(constant.VIEW.gallery)
+
+
+})
 
 router.get(constant.ROUTE.loginUser, (req, res) => {
     res.status(200).render(constant.VIEW.loginUser);
 })
 
-router.get(constant.ROUTE.userAccount, (req, res) => {
-    res.status(200).render(constant.VIEW.userAccount);
+router.post(constant.ROUTE.loginUser, async (req, res) => {
+    const user = await UserInfoModel.findOne({
+        userMail: req.body.userMail
+    });
+    if (!user) res.redirect(constant.ROUTE.createUser)
+
+    const validUser = await bcrypt.compare(req.body.userPassWord, user.userPassWord)
+    console.log(validUser)
+    if (validUser) return res.redirect(
+        constant.VIEW.userAccount)
+    res.redirect(constant.ROUTE.loginUser)
+
 })
 
-router.get(constant.ROUTE.checkout, (req, res) => {
-    res.status(200).render(constant.VIEW.checkout);
+router.get(constant.ROUTE.userAccount, async (req, res) => {
+    const showUserInfo = await UserInfoModel.findOne();
+    console.log(showUserInfo)
+    res.status(200).render(constant.VIEW.userAccount, {
+        showUserInfo
+    });
 })
+
+
+
 
 router.get(constant.ROUTE.confirmation, (req, res) => {
     res.status(200).render(constant.VIEW.confirmation);
