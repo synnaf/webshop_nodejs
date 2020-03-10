96e90662f66b525537899b2fdd2dc3028832eb5d
[synnaf] <[f.vforsman@gmail.com]>
2020-03-10 14:09:25 +0100

    Aktiverat errorRoute i server.js

--- routes/errorRoute.js
+++ routes/errorRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const constant = require('../constant');
 
 router.get(constant.ROUTE.error, (req, res) => {
-    res.status(404).render(constant.VIEW.error, {errmsg: '404'});
+    res.status(404).render(constant.VIEW.error, {errmsg: '404. Sidan finns inte!'});
 })
 
 module.exports = router;
