2165e666777e77da6f82574fd558d0585c723dbd
Vic564 <victor.a.persson@gmail.com>
2020-03-12 22:59:04 +0100

    redirect och query-validering i /gallery url

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,29 +2,46 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../model/product');
 const constant = require('../constant');
+const url = require('url');
 
 router.get(constant.ROUTE.index, (req, res) => {
     res.render(constant.VIEW.index, {});
 })
 
 router.get(constant.ROUTE.gallery, async (req, res) => {
-    const productPerPage = 3;
+    if (req.query.page === undefined) {
+        res.redirect(url.format({
+            pathname: constant.ROUTE.gallery,
+            query: {
+                "page": 1
+            }
+        }));
+    }
+    const productPerPage = 4;
     const page = +req.query.page;
     const productAmount = await Product.find().countDocuments();
-    const productList = await Product.find().skip(productPerPage * (page - 1)).limit(productPerPage);
-    res.render(constant.VIEW.gallery, {
-        productList,
-        productAmount,
-        currentPage: page,
-        isFirst: page <= 1,
-        isSecond: page == 2,
-        isLast: page === Math.ceil(productAmount / productPerPage),
-        isSecondLast: page === (Math.ceil(productAmount / productPerPage) - 1),
-        nextPage: page + 1,
-        previousPage: page - 1,
-        lastPage: Math.ceil(productAmount / productPerPage),
-        productListRoute: constant.ROUTE.gallery
-    });
+    if (Number.isInteger(page) && (page >= 1) && (page <= Math.ceil(productAmount / productPerPage))) {
+        const productList = await Product.find().skip(productPerPage * (page - 1)).limit(productPerPage);
+        res.render(constant.VIEW.gallery, {
+            productList,
+            productAmount,
+            currentPage: page,
+            isFirst: page <= 1,
+            isSecond: page == 2,
+            isLast: page === Math.ceil(productAmount / productPerPage),
+            isSecondLast: page === (Math.ceil(productAmount / productPerPage) - 1),
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(productAmount / productPerPage),
+            productListRoute: constant.ROUTE.gallery
+        });
+    } else {
+        res.redirect(url.format({
+            pathname: constant.ROUTE.error,
+            query: {}
+        }));
+    }
+    
 })
 
 router.get(constant.ROUTE.product, async (req, res) => {
