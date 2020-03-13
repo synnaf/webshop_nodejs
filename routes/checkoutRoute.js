f4fe51884697d69d0e2028deb4321de253f1418e
maria ekman <mariaekmaan@hotmail.com>
2020-03-14 00:26:48 +0100

    verifyToken till checkout

--- routes/checkoutRoute.js
+++ routes/checkoutRoute.js
@@ -2,20 +2,16 @@ const express = require('express');
 const router = express.Router();
 const constant = require('../constant');
 const userModel= require("../model/user")
+const verifyToken = require("./verifyToken")
 
- //href="token"
- //if börjar här
+
     router.get(constant.ROUTE.checkout, async (req, res) => {
-       
-     // hämtar token från  verifytoken;   hämtar token från  måste recuired
-    
     const token=req.cookies.jsonwebtoken 
     console.log(token,"finns på checkout")
     const showUserInfo = await  userModel.findOne()
    
-  
     if(token){
-        return res.status(202).render(constant.VIEW.checkout,{showUserInfo})
+        return verifyToken, res.status(202).render(constant.VIEW.checkout,{showUserInfo})
        
     } else{
        
