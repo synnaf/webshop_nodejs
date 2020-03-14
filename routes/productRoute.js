ae34e1956cda201c2b04e40fba9426f91c1b35c8
Vic564 <victor.a.persson@gmail.com>
2020-03-14 10:25:43 +0100

    fixat liten bugg med redirect

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -16,35 +16,34 @@ router.get(constant.ROUTE.gallery, async (req, res) => {
                 "page": 1
             }
         }));
-    }
-    const productPerPage = 4;
-    const page = +req.query.page;
-    const productAmount = await Product.find().countDocuments();
-    const pageAmount = Math.ceil(productAmount / productPerPage);
-    if (Number.isInteger(page) && (page >= 1) && (page <= pageAmount)) {
-        const productList = await Product.find().skip(productPerPage * (page - 1)).limit(productPerPage);
-        res.render(constant.VIEW.gallery, {
-            productList,
-            productAmount,
-            currentPage: page,
-            isFirst: page <= 1,
-            isSecond: page === 2,
-            isLast: page === pageAmount,
-            isSecondLast: page === (pageAmount - 1),
-            nextPage: page + 1,
-            previousPage: page - 1,
-            lastPage: pageAmount,
-            productListRoute: constant.ROUTE.gallery
-        });
     } else {
-        res.redirect(url.format({
-            pathname: constant.ROUTE.error,
-            query: {}
-        }));
+        const productPerPage = 4;
+        const page = +req.query.page;
+        const productAmount = await Product.find().countDocuments();
+        const pageAmount = Math.ceil(productAmount / productPerPage);
+        if (Number.isInteger(page) && (page >= 1) && (page <= pageAmount)) {
+            const productList = await Product.find().skip(productPerPage * (page - 1)).limit(productPerPage);
+            res.render(constant.VIEW.gallery, {
+                productList,
+                productAmount,
+                currentPage: page,
+                isFirst: page <= 1,
+                isSecond: page === 2,
+                isLast: page === pageAmount,
+                isSecondLast: page === (pageAmount - 1),
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: pageAmount,
+                productListRoute: constant.ROUTE.gallery
+            });
+        } else {
+            res.redirect(url.format({
+                pathname: constant.ROUTE.error,
+                query: {}
+            }));
+        }
     }
-    
 })
-
 router.get(constant.ROUTE.product, async (req, res) => {
     const oneProduct = await Product.findById({ _id: req.params.id });
     res.render(constant.VIEW.product, { oneProduct });
