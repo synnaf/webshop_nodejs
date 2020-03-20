cc3060c2f10f149abe4d26d89a6b7425f2058381
[synnaf] <[f.vforsman@gmail.com]>
2020-03-20 21:50:44 +0100

    Nu fungerar inlogg/utlogg, men något blir fel med checkout och varukorgemn

--- routes/checkoutRoute.js
+++ routes/checkoutRoute.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { ROUTE, VIEW, PRODUCT } = require('../constant');
+const { ROUTE, VIEW } = require('../constant');
 const userModel = require("../model/user")
 const verifyToken = require("./verifyToken")
 
 
 router.get(ROUTE.checkout, verifyToken, async (req, res) => {
+    const token = req.cookies.jsonwebtoken;  
 
-    // const token = req.cookies.jsonwebtoken 
-    // const user = await UserInfoModel.findOne({_id: req.body.userInfo._id});
-
-    if (verifyToken) {
+    if (token) {
         const showUserInfo = await userModel.findOne({ _id: req.body.userInfo._id })
         res.status(202).render(VIEW.checkout, { ROUTE, showUserInfo: showUserInfo.shoppingcart })
-
     } else {
         return res.status(202).render(VIEW.checkout, {
             ROUTE,
