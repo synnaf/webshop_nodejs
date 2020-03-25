891483658601dc32a7641e2d63805743e758331e
marcusgardh <58325557+marcusgardh@users.noreply.github.com>
2020-03-25 10:24:33 +0100

    Lite routing-test och cypress-test

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const mongoDB ={
-    databaseUrl: process.env.MONGO_DB
+    databaseUrl: process.env.MONGO_DB || "mongodb+srv://pizza_2019:pizza_2019@cluster0-17ron.mongodb.net/product?retryWrites=true&w=majority"
 } 
 
 const spotify ={
